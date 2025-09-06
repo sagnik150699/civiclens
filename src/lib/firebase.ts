43ea2e5e25c820adae96e5cc81833859359b7d70
SAGNIK BHATTACHARYA <SagnikBhattacharya1506@gmail.com>
2025-09-06 20:18:44 +0000

    I have downloaded  the generate new private key. I am uploading it here.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,9 +5,9 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig: FirebaseOptions = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  projectId: "civiclens-bexm4",
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  storageBucket: "civiclens-bexm4.appspot.com",
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
