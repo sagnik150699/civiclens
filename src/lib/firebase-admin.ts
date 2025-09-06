aed4b14eec33a656a5d71e08e6ad3a2365f60bbd
SAGNIK BHATTACHARYA <SagnikBhattacharya1506@gmail.com>
2025-09-06 18:47:25 +0000

    go

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,15 +1,12 @@
+
 import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
-    try {
-        admin.initializeApp({
-            credential: admin.credential.applicationDefault(),
-            databaseURL: `https://${process.env.GCLOUD_PROJECT}.firebaseio.com`,
-            storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        });
-    } catch (error) {
-        console.log('Firebase admin initialization error', error);
-    }
+    admin.initializeApp({
+        credential: admin.credential.applicationDefault(),
+        databaseURL: `https://${process.env.GCLOUD_PROJECT}.firebaseio.com`,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    });
 }
 
 export const adminDb = admin.firestore();
