e3607e226d0f8ecef0559b4c1028d0b5059b37f2
SAGNIK BHATTACHARYA <SagnikBhattacharya1506@gmail.com>
2025-09-19 19:42:49 +0000

    Try fixing this error: `Console Error: FirebaseError: Expected first arg

--- src/lib/server/firebase-admin.ts
+++ src/lib/server/firebase-admin.ts
@@ -4,21 +4,14 @@ import { getFirestore, type Firestore } from 'firebase-admin/firestore';
 import { getStorage, type Storage } from 'firebase-admin/storage';
 
 let app: App;
-let db: Firestore;
-let bucket: Storage['bucket'];
 
-try {
-  if (!getApps().length) {
-    app = initializeApp();
-  } else {
-    app = getApp();
-  }
-
-  db = getFirestore(app);
-  bucket = getStorage(app).bucket(process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET);
-  
-} catch (error) {
-    console.error('Firebase Admin initialization failed:', error);
+if (!getApps().length) {
+  app = initializeApp();
+} else {
+  app = getApp();
 }
+
+const db: Firestore = getFirestore(app);
+const bucket: Storage['bucket'] = getStorage(app).bucket(process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET);
 
 export { db, bucket };
