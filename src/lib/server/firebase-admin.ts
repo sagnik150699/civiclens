38c7d5255a052b52af39f02566169be27cafbea8
SAGNIK BHATTACHARYA <SagnikBhattacharya1506@gmail.com>
2025-09-19 19:45:41 +0000

    Try fixing this error: `Runtime Error: Error: Bucket name not specified

--- src/lib/server/firebase-admin.ts
+++ src/lib/server/firebase-admin.ts
@@ -1,11 +1,8 @@
 
-'use server';
-
-import { initializeApp, getApps, getApp, type App, type ServiceAccount } from 'firebase-admin/app';
+import { initializeApp, getApps, getApp, type App } from 'firebase-admin/app';
 import { getFirestore, type Firestore } from 'firebase-admin/firestore';
 import { getStorage, type Storage } from 'firebase-admin/storage';
 import { firebaseConfig } from '../firebase-client';
-
 
 let app: App;
 
@@ -16,13 +13,12 @@ if (!getApps().length) {
             clientEmail: `firebase-adminsdk-v59j3@${firebaseConfig.projectId}.iam.gserviceaccount.com`,
             privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
         },
-        storageBucket: firebaseConfig.storageBucket,
     });
 } else {
   app = getApp();
 }
 
 const db: Firestore = getFirestore(app);
-const bucket: Storage['bucket'] = getStorage(app).bucket();
+const bucket: Storage['bucket'] = getStorage(app).bucket('civiclens-bexm4.appspot.com');
 
 export { db, bucket };
